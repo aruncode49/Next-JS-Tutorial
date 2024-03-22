4975a0c02cb25a5c3c0c1087aeeb4eb7f52413d3
Arun Kumar <aruncode49@gmail.com>
2024-03-22 17:15:14 +0530

    link tag

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello</h1>
+      <Link href={"/user/3"}>Go to User</Link>
     </main>
   );
 }
